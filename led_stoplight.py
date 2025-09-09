8d09de7ce3f74c25e5d55259b7a65bbd72a2e43b
Ronnoc1981 <connorh0113@gmail.com>
2025-09-09 13:10:28 -0600

    Add files via upload
    Lab 6 part 1 stuff

---
+++ led_stoplight.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(16,GPIO.OUT)
+GPIO.setup(21,GPIO.OUT)
+
+print ("stoplight on")
+# green light code
+GPIO.output(16,GPIO.HIGH)
+time.sleep(5)
+GPIO.output(16,LOW)
+# yellow light code
+GPIO.output(21,GPIO.HIGH)
+time.sleep(1)
+GPIO.output(21,GPIO.LOW)
+# red light code
+GPIO.output(18,GPIO.HIGH)
+time.sleep(4)
+GPIO.output(18,GPIO.LOW)
+print ("stoplight off")
